ce96b5d02e557cb05010ff822e9d55faa438f84c
wendelllopess <wendelllopes020@gmail.com>
2023-12-26 18:31:10 -0300

    comecando JS

---
+++ Cadastro.js
@@ -0,0 +1,10 @@
+const formulario = document.getElementById("formulario");
+
+let nome = document.getElementById("nome");
+let cpf = document.getElementById("cpf");
+let email = document.getElementById("email");
+let telefone = document.getElementById("telefone");
+let senha = document.getElementById("senha");
+let senhaConfirmacao = document.getElementById("senhaConfirmacao");
+
+                                                                                                                                                                                        
