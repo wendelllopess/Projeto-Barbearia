81d832cc69fb89fe897382cacdb09ea290090a45
wendelllopess <wendelllope020@gmail.com>
2024-01-26 13:55:30 -0300

    iniciando a logica

--- Cadastro.js
+++ Cadastro.js
@@ -1,10 +1,53 @@
 const formulario = document.getElementById("formulario");
+let botaoCadastro = document.getElementById('botaoCadastro');
 
-let nome = document.getElementById("nome");
-let cpf = document.getElementById("cpf");
-let email = document.getElementById("email");
-let telefone = document.getElementById("telefone");
-let senha = document.getElementById("senha");
-let senhaConfirmacao = document.getElementById("senhaConfirmacao");
+formulario.addEventListener("submit", (event)=>{
+    event.preventDefault();
 
-                                                                                                                                                                                        
+    if(validarFormulario){
+        cadastraUsuario();
+
+        setTimeout(()=>{
+            window.location.href="./login.html"
+        }, 2000)
+        alert("Usuario cadastrado com sucesso!")
+    } else{
+        alert("O formulario presisa ser preenchido!")
+    }
+   
+})
+
+function cadastraUsuario(){
+    let nome = document.getElementById("nome").value;
+    let cpf = document.getElementById("cpf").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let senha = document.getElementById("senha").value;
+    let senhaConfirmacao = document.getElementById("senhaConfirmacao").value;
+   
+    if(senha !== senhaConfirmacao){
+        alert("As senhas devem ser iguais!")
+        return;
+    }
+
+    localStorage.setItem("nome", nome); 
+    localStorage.setItem("cpf", cpf);
+    localStorage.setItem("email", email);
+    localStorage.setItem("telefone", telefone);
+    localStorage.setItem("senha", senha);
+    localStorage.setItem("senhaConfirmacao", senhaConfirmacao);
+}
+
+function validarFormulario(){
+    if(nome.value === '' || cpf.value === '' || email.value === '' || telefone.value === '' || senha.value === '' || senhaConfirmacao.value === ''){
+        return false;
+    }else if(senha!==senhaConfirmacao){
+        alert("A senha devem ser iguais")
+        return cadastraUsuario;
+    }
+    else{
+        return true
+    }
+}
+
+botaoCadastro.addEventListener('click', cadastraUsuario());
